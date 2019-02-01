40961123036a0e79d9cb78482fdcd08d5f317e49
najitestech <42984888+najitestech@users.noreply.github.com>
2019-02-01 13:17:46 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "user_name" {
+  default = "administrator@vsphere.local"
+}
+
+variable "password" {
+  default = "Password"
+}
+
+variable "vsphere_server" {
+  default = "192.168.0.207"
+}
+
+variable "ip_address" {
+  default = "192.168.0.102"
+}
+
+variable "master_gateway" {
+  default = "192.168.0.1"
+}
+
+variable "gateway" {
+  default = "24"
+}
